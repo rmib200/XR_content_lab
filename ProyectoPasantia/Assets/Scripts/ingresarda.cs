e64524822acaa2a4c36e3be17bfb8f05ea5b0134
karen-velasquez <maritza.karen.flores.velasquez@gmail.com>
2021-09-12 20:48:46 -0400

    aumentando docs python y lectura de informaciond del Neo4j

--- ProyectoPasantia/Assets/Scripts/ingresarda.cs
+++ ProyectoPasantia/Assets/Scripts/ingresarda.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Collections.Generic;
 
 
 
@@ -27,6 +28,7 @@ public class ingresarda : MonoBehaviour
     public InputField sexoField;
     public InputField ciField;
     public InputField dptoField;
+    public InputField textoBusqueda;
     public Text busco;
 
     public string resultado="";
@@ -38,13 +40,11 @@ public class ingresarda : MonoBehaviour
 
 
     public void guardarDatos(){
-       running=false;
        guardar=true;
     }
 
     
     public void buscarDatos(){
-       running=false;
        buscar=true;
     }
 
@@ -62,10 +62,32 @@ public class ingresarda : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         busco.text = "respuesta++++"+resultado;
+        if(buscar==true){
+               busco.text = "respuesta++++"+resultado;
+        }
         //transform.position = receivedPos; 
     }
      
+     /*Realizando la conexion con Python*/
+    public bool coneccionPython(){
+        NetworkStream nwStream = client.GetStream();
+        byte[] buffer = new byte[client.ReceiveBufferSize];
+
+        //---receiving Data from the Host----
+        int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize); //Getting data in Bytes from Python
+        string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead); //Converting byte data to string
+
+        if (dataReceived != null)
+        { 
+            print("Conexion: " +dataReceived);
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+
      void GetInfo()
      {
          localAdd=IPAddress.Parse(connectionIP);
@@ -74,16 +96,14 @@ public class ingresarda : MonoBehaviour
 
          client=listener.AcceptTcpClient();
 
-         running=true;
-         while(running!=false){
-            print("Esperando....");
-
-         }
-         if(running==false && guardar==true){
-             ingresarDatos();
-         }else if(running==false && buscar==true){
-             buscarlosDatos();
-         }
+        while(true){
+            if(guardar==true){
+                ingresarDatos();
+                
+            }else if(buscar==true){
+                buscarlosDatos();
+            }
+        }
          listener.Stop();
      }
 
@@ -123,6 +143,7 @@ public class ingresarda : MonoBehaviour
             int read;
             do
             {
+
               read = nwStream.Read(responseBuffer, 0, client.ReceiveBufferSize);
             } while (read > 0);
 
@@ -135,72 +156,106 @@ public class ingresarda : MonoBehaviour
         }
     }
 
-     void buscarlosDatos(){
+     void buscarlosDatos(){    
+        string busq=textoBusqueda.text;
+
+        if(busq!=""){
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+            //---receiving Data from the Host----
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize); //Getting data in Bytes from Python
+            string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead); //Converting byte data to string
+
+      
+            if (dataReceived != null)
+            {
+                //---Sending Data to Host----
+                byte[] myWriteBuffer = Encoding.ASCII.GetBytes("MATCH(x) WHERE x.nombre='"+busq+"' return (x)"); //Converting string to byte data
+                nwStream.Write(myWriteBuffer, 0, myWriteBuffer.Length); //Sending the data in Bytes to Python
+        
+                //---receiving Data from the Host----
+                int bytes = nwStream.Read(buffer, 0, client.ReceiveBufferSize); //Getting data in Bytes from Python
+                string data = Encoding.UTF8.GetString(buffer, 0, bytes); //Converting byte data to string
+                print(data);
+                if(data!=null && data!="NO"){
+                    string word1 = "properties={";
+                    string word2 = "}";
+                    string text = stringBetween(data, word1, word2);
+                    print(text);
+                    resultado=text+"";
+                }else{
+                    resultado="no se encontro el valor";
+                }
+                if(data=="NO"){
+                        resultado="no se encontro el valor";
+                }
+           
+          
+            }
+               buscar=false;
+        }else{
+            resultado="No se puede meter dato vacio";
+        }
+    }
+
+
+
+        //Obteniendo el string que se encuentra entre dos elementos   
+        public static string stringBetween(string Source, string Start, string End)
+        {
+            string resultado="";
+            string result = "";
+            if (Source.Contains(Start) && Source.Contains(End))
+            {
+                int StartIndex = Source.IndexOf(Start, 0) + Start.Length;
+                int EndIndex = Source.IndexOf(End, StartIndex);
+                result = Source.Substring(StartIndex, EndIndex - StartIndex);
+
+                List<string> objetos=new List<string>();
+                objetos=divisionsplit(result);
+                for (int i = 0; i < objetos.Count; i++) 
+                    {
+                         resultado+="\n"+objetos[i];
+                         
+                    }   
+
+                return resultado;
+            }
+
+            return result;
+        }
+
+        //descomprimir los datos de properties con split
+        public static List<string> divisionsplit(string properties){
+           List<string> objetos=new List<string>();
+           string[] propiedades=properties.Split(',');
+           foreach(string propi in propiedades)
+                objetos.Add(propi);
+            
+            return objetos;
+        
+        }
+
+
+
+
+
+
+
      
-        NetworkStream nwStream = client.GetStream();
-        byte[] buffer = new byte[client.ReceiveBufferSize];
-
-        //---receiving Data from the Host----
-        int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize); //Getting data in Bytes from Python
-        string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead); //Converting byte data to string
-
-      
-        if (dataReceived != null)
-        {
-            //---Using received data---
-          //  receivedPos = StringToVector3(dataReceived); //<-- assigning receivedPos value from Python
-              print("Conexion: "+dataReceived);
-
- 
-
-              //---Sending Data to Host----
-             byte[] myWriteBuffer = Encoding.ASCII.GetBytes("MATCH(x) return (x) "); //Converting string to byte data
-             nwStream.Write(myWriteBuffer, 0, myWriteBuffer.Length); //Sending the data in Bytes to Python
-        
-            
-
-            //---receiving Data from the Host----
-             int bytes = nwStream.Read(buffer, 0, client.ReceiveBufferSize); //Getting data in Bytes from Python
-             string data = Encoding.UTF8.GetString(buffer, 0, bytesRead); //Converting byte data to string
-             print(data);
-             resultado=data;
-            
-
-        }
-    }
 
 
 
-      void SendAndReceiveData()
-    {
-        NetworkStream nwStream = client.GetStream();
-        byte[] buffer = new byte[client.ReceiveBufferSize];
-
-        //---receiving Data from the Host----
-        int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize); //Getting data in Bytes from Python
-        string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead); //Converting byte data to string
-
-      
-        if (dataReceived != null)
-        {
-            //---Using received data---
-          //  receivedPos = StringToVector3(dataReceived); //<-- assigning receivedPos value from Python
-              print("Conexion: "+dataReceived);
 
 
-              //---Sending Data to Host----
-             byte[] myWriteBuffer = Encoding.ASCII.GetBytes("MATCH(x) WHERE x.name='Barcelona' return (x) LIMIT 3"); //Converting string to byte data
-             nwStream.Write(myWriteBuffer, 0, myWriteBuffer.Length); //Sending the data in Bytes to Python
-        
-            
 
-            //---receiving Data from the Host----
-             int bytes = nwStream.Read(buffer, 0, client.ReceiveBufferSize); //Getting data in Bytes from Python
-             string data = Encoding.UTF8.GetString(buffer, 0, bytesRead); //Converting byte data to string
-             print("sera este el fin del hombre ara;a?????"+ data);
 
-        }
-    }
+
+
+
+
+
+
 
 
     public static string GetLocalIPAddress()
